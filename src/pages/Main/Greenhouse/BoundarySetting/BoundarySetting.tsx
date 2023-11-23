6dfb1bd70168732d155e3d77adb6e093af718e4a
AzureAD\陳品肇 <brookchen@chase.com.tw>
2023-11-23 16:47:33 +0800

    add echart lib

--- src/pages/Main/Greenhouse/BoundarySetting/BoundarySetting.tsx
+++ src/pages/Main/Greenhouse/BoundarySetting/BoundarySetting.tsx
@@ -2,7 +2,7 @@ import { FC, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import GreenhouseService from "../../../../services/greenhouse/greenhouseService";
 import GreenhouseQuery from "../../../../services/greenhouse/queries";
-
+import ReactEcharts from "echarts-for-react";
 interface BoundarySettingProps { }
 
 const BoundarySetting: FC<BoundarySettingProps> = () => {
@@ -19,27 +19,48 @@ const BoundarySetting: FC<BoundarySettingProps> = () => {
     避免返回其他頁面，又立馬進入同一頁，導致實體被產生兩次。
     第一次進入會先將實體建立->摧毀->建立，好讓下次進入時，以確保上一個實體已結束
     cleanup 函式
-    */ 
+    */
 
-    const {data} = GreenhouseQuery.useGetBoundaryList(params.ghgId || '');
-    console.log(data);
-    // useEffect(() => {
-    //     POST(`greenhouse/${params.ghgId}/boundarysetting/list`,
-    //         searchParam)
-    //         .then(x => {
-    //             if (x) {
-    //                 // console.log(x);
-    //             }
-    //         })
+    // const {data} = GreenhouseQuery.useGetBoundaryList(params.ghgId || '');
+    // console.log(data);
+    useEffect(() => {
+        GreenhouseService.getBoundarylist(params.ghgId || '')
+            .then(x => {
+                if (x) {
+                    // console.log(x);
+                }
+            })
 
-    //         console.log('取得api')
-    //     return ()=>{
-    //         console.log('摧毀');
-    //     }
-    // }, [])
+        console.log('取得api')
+        return () => {
+            console.log('摧毀');
+        }
+    }, []);
 
-    const handleBack = ()=>{
+    /** 返回頁面 */
+    const handleBack = () => {
         navigate('/main/greenhouse/list');
+    }
+
+    /** echart 設置 */
+    const BarChart = () => {
+        let option = {
+            xAxis: {
+                type: 'category',
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [
+                {
+                    data: [120, 200, 150, 80, 70, 110, 130],
+                    type: 'bar'
+                }
+            ]
+        };
+
+        return <ReactEcharts option={option} />;
     }
     return (<>
         <button onClick={handleBack}>
@@ -47,6 +68,10 @@ const BoundarySetting: FC<BoundarySettingProps> = () => {
         </button>
         <div>
             ghgId: {params.ghgId}
+        </div>
+        <div>
+            <span>echart 範例</span>
+            <BarChart/>
         </div>
     </>)
 }
